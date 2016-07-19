c53f0659e02d23e30cb8933a74b53e5958b76207
Charles Cosse <ccosse@gmail.com>
2016-07-19 16:43:47 -0700

    delete all

--- about.js
+++
@@ -1,3 +0,0 @@
-about="<img src=\"application-exit.png\" onclick=\"dismiss_glasspaneCB()\"></img><br><center><h3>French Flashcard System</h3></center><p>Website: <a href=\"http://www.asymptopia.org\" target=_blank >Asymptopia Software</a><br>Author: Charles B. Cossé<br>Email: ccosse@asymptopia.org</p>";  
-
-
